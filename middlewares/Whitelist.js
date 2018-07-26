1bb8e64332001c35dc684586c9d2bc0b4f2f1419
Constantin Tsukanov <c@indotech.ru>
2018-07-26 19:39:42 +0300

    Add whitelist middleware

---
+++ middlewares/Whitelist.js
@@ -0,0 +1,121 @@
+const isFunction = require('lodash/isFunction');
+const isBoolean = require('lodash/isBoolean');
+const isObject = require('lodash/isObject');
+const set = require('lodash/set');
+
+/**
+ * Whitelist middleware for the small-rpc
+ * Create instance with whitelist array/map/object
+ * @class Whitelist
+ * @prop {Object} tree — whitelist tree
+ * @param {(Array|Map|Object)} list — array of paths. Map of (key: path, value: boolen or function with boolean result). Object of (key: path, value: boolen or function with boolean result)
+ */
+class Whitelist {
+  constructor(list) {
+    this.tree = {};
+    this.parseList(list, false);
+  }
+
+  /**
+   * reset tree field
+   * @param {Boolean} [isReset=true] reset or not
+   */
+  resetTree(isReset = true) {
+    this.tree = isReset ? {} : this.tree;
+  }
+
+  /**
+   * parse new lits
+   * @param {(Array|Map|Object)} list — array of paths. Map of (key: path, value: boolen or function with boolean result). Object of (key: path, value: boolen or function with boolean result)
+   * @param  {Boolean} [force=true] reset old tree or not
+   */
+  parseList(list, force = true) {
+    this.resetTree(force);
+    if (Array.isArray(list)) return this.parseArrayList(list);
+    if (list instanceof Map) return this.parseMapList(list);
+    if (isObject(list)) return this.parseObjectList(list);
+  }
+
+  /**
+   * parse new list from map
+   * @param {Map} list — map of (key: path, value: boolen or function with boolean result)
+   */
+  parseMapList(list) {
+    for (const [key, value] of list) {
+      set(this.tree, key, value);
+    }
+  }
+
+  /**
+   * parse new list from object
+   * @param {Object} list — object of (key: path, value: boolen or function with boolean result)
+   */
+  parseObjectList(list) {
+    for (const [key, value] of Object.entries(list)) {
+      set(this.tree, key, value);
+    }
+  }
+
+  /**
+   * parse new list
+   * @param {Array} list — array of paths
+   */
+  parseArrayList(list) {
+    for (const key of list) {
+      set(this.tree, key, true);
+    }
+  }
+
+  /**
+   * run one node of whitelist tree
+   * @param  {Mixed} node     current node of tree
+   * @param  {Mixed} payload  rpc request payload
+   * @param  {Object} action  action of rpc
+   * @param  {RPC} rpc        instance
+   * @return {(Boolean|undefined)} boolean result or undefined, if node has childs
+   */
+  runNode(node, payload, action, rpc) {
+    if (!node) return false;
+    if (isFunction(node)) return !!node(payload, action, rpc);
+    if (isFunction(node.call)) return !!node.call(payload, action, rpc);
+    if (!isObject(node)) return true;
+  }
+
+  /**
+   * get result of middleware
+   * @param  {Mixed} payload  rpc request payload
+   * @param  {Object} action  action of rpc
+   * @param  {RPC} rpc        instance
+   * @return {Boolean}        stop or not
+   */
+  getResult(payload, action, rpc) {
+    const path = rpc.makePathFromAction(action, true, true);
+    // all section
+
+    let node;
+    for (const step of path) {
+      if (!node) node = this.tree[step];
+      else node = node[step];
+
+      if (node === undefined) return false;
+      const result = this.runNode(node, payload, action, rpc);
+      if (isBoolean(result)) return result;
+    }
+    // for safe
+    return false;
+  }
+
+  /**
+   * call of middleware
+   * you can override this method if you want throw errors with stop
+   * @param  {Mixed} payload  rpc request payload
+   * @param  {Object} action  action of rpc
+   * @param  {RPC} rpc        instance
+   * @return {Boolean}        stop or not
+   */
+  call(payload, action, rpc) {
+    return this.getResult(payload, action, rpc);
+  }
+}
+
+module.exports = Whitelist;
---
+++ middlewares/Whitelist.test.js
@@ -0,0 +1,73 @@
+/* global test expect */
+
+const RPC = require('../RPC');
+const Whitelist = require('./Whitelist');
+
+test('Whitelist using', async () => {
+  const whitelist = new Whitelist([
+    'add.john'
+  ]);
+  const rpc = new RPC();
+
+  let counter = 0;
+  const add = () => counter += 1;
+
+  const mainLib = {
+    user: { hi: add, getPassword: add },
+    group: { hi: add, getSecret: add }
+  }
+
+  const addLib = {
+    me: { hi: add },
+    john: { hi: add, bye: add }
+  }
+
+  rpc.use(whitelist);
+  rpc.setLib(mainLib);
+  rpc.setLib('add', addLib);
+
+  // ok
+  await rpc.call({}, { lib: 'add', module: 'john', method: 'hi' });
+  // ok
+  await rpc.call({}, { lib: 'add', module: 'john', method: 'bye' });
+
+  expect(counter).toBe(2);
+  counter = 0;
+
+  whitelist.parseList({ main: { user: { hi: true } } }, false);
+
+  // ok
+  await rpc.call({}, { lib: 'add', module: 'john', method: 'hi' });
+  // ok
+  await rpc.call({}, { lib: 'add', module: 'john', method: 'bye' });
+  // ok
+  await rpc.call({}, { module: 'user', method: 'hi' });
+  // not ok
+  await rpc.call({}, { module: 'user', method: 'getPassword' });
+
+  expect(counter).toBe(3);
+  counter = 0;
+
+  whitelist.parseList(new Map([
+    ['main.user.hi', true],
+    ['main.group', (payload, action) => !!action.token],
+    ['add', true],
+    // should replace “add: true” record
+    ['add.me.hi', false]
+  ]));
+  // not ok
+  await rpc.call({}, { lib: 'add', module: 'me', method: 'hi' });
+  // not ok
+  await rpc.call({}, { module: 'group', method: 'getSecret' });
+  // ok
+  await rpc.call({}, { module: 'group', method: 'getSecret', token: true });
+  // ok
+  await rpc.call({}, { module: 'group', method: 'hi', token: true });
+  // not ok
+  await rpc.call({}, { module: 'user', method: 'getPassword' });
+  // ok
+  await rpc.call({}, { module: 'user', method: 'hi' });
+  await rpc.call({}, { lib: 'invalid', module: 'invalid', method: 'invalid' });
+  expect(counter).toBe(3);
+  counter = 0;
+});
